e7bea0f292b892fe8c29798790d39f197979607c
kevin collier <collier.kevin.m@gmail.com>
2023-08-06 12:22:16 +0900

    added interest and points to user table in schema

---
+++ db/migrate/20230806031703_add_interest_to_users.rb
@@ -0,0 +1,5 @@
+class AddInterestToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :interest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_112852) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_032004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_112852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "interest"
+    t.integer "points", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
