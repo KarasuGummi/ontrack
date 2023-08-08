6fc3b0c5e24c562d4c5f38754ce867175a274ae3
kevin collier <collier.kevin.m@gmail.com>
2023-08-08 20:53:28 +0900

    ran the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_054727) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_114933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_054727) do
     t.index ["user_id"], name: "index_buddies_on_user_id"
   end
 
+  create_table "interests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "deadline"
@@ -91,6 +97,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_054727) do
     t.index ["user_id"], name: "index_user_answers_on_user_id"
   end
 
+  create_table "user_interests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "interest_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["interest_id"], name: "index_user_interests_on_interest_id"
+    t.index ["user_id"], name: "index_user_interests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -100,7 +115,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_054727) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "interest"
     t.integer "points", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -114,4 +128,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_054727) do
   add_foreign_key "questions", "projects"
   add_foreign_key "user_answers", "answers"
   add_foreign_key "user_answers", "users"
+  add_foreign_key "user_interests", "interests"
+  add_foreign_key "user_interests", "users"
 end
