00ce2f5c33fe1fd1d89f28cb85d8b2bc14eb985e
kevin collier <collier.kevin.m@gmail.com>
2023-08-18 11:16:21 +0900

    created loading-modal css

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -8,10 +8,14 @@ export default class extends Controller {
   }
   toggleDogImage() {
     setInterval(() => {
-      if (this.dogImageTarget.getAttribute("src") === "/path/to/first/image.png") {
-        this.dogImageTarget.setAttribute("src", "/path/to/second/image.png");
+      const currentImage = this.dogImageTarget.querySelector('img').getAttribute("src");
+      const firstImage = 'https://res.cloudinary.com/du53mgiot/image/upload/v1691390417/gsv9qtfodjbhccbmhwws.png';
+      const secondImage = 'https://res.cloudinary.com/du53mgiot/image/upload/v1691390436/qifldk91fout1jdwru7r.png';
+
+      if (currentImage === firstImage) {
+        this.dogImageTarget.innerHTML = `<img src="${secondImage}" alt="Animated Dog" height="140" style="crop: fill;">`;
       } else {
-        this.dogImageTarget.setAttribute("src", "/path/to/first/image.png");
+        this.dogImageTarget.innerHTML = `<img src="${firstImage}" alt="Animated Dog" height="140" style="crop: fill;">`;
       }
     }, 500);
   }
