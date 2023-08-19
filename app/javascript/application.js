41fbaf10a197bcd79f41564a12bbacb61ad4004f
Juri Kato <dance.luv48@gmail.com>
2023-08-19 09:42:45 +0900

    removed javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import BuddyimgController from "./buddyimg_controller";
-application.register("buddyimg", BuddyimgController);
 
