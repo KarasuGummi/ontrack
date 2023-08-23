68795d60599d687dba79cd652337bb5feac7a026
kevin collier <collier.kevin.m@gmail.com>
2023-08-23 21:10:59 +0900

    fixed javascript for flipping cards

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,17 +2,15 @@ class QuestionsController < ApplicationController
   before_action :set_project
 
   def index
-    message = params[:message]
-    @response = OpenaiService.new(message).call
     @questions = @project.questions
     @buddy = current_user.buddy
   end
 
-  def show
-    question = Question.find(params[:id])
-    @answers = @question.answers
-    # question = params[:question]
-  end
+  # def show
+  #   question = Question.find(params[:id])
+  #   @answers = @question.answers
+  #   # question = params[:question]
+  # end
 
   private
 
