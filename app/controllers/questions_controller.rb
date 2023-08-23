be99ecf081697d705db8a2c7653fcc9caf5f15d0
kevin collier <collier.kevin.m@gmail.com>
2023-08-23 21:34:40 +0900

    added logic to the controller for user to receive points for visiting the flashcard page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,14 @@ class QuestionsController < ApplicationController
   def index
     @questions = @project.questions
     @buddy = current_user.buddy
+
+    unless session[:awarded_points_for_questions]
+      random_points = rand(5..10)
+      current_user.earn_points(random_points)
+
+      flash[:notice] = "Here's #{random_points} points. Now go on and do your best!"
+      session[:awarded_points_for_questions] = true
+    end
   end
 
   # def show
