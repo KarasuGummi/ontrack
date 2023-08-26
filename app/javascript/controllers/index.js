6c30eb6d288a27585031c31869b26f1f65b2bf25
Juri Kato <dance.luv48@gmail.com>
2023-08-26 09:12:55 +0900

    removed feed controller.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import ChatroomSubscriptionController from "./chatroom_subscription_controller"
 application.register("chatroom-subscription", ChatroomSubscriptionController)
 
-import FeedController from "./feed_controller"
-application.register("feed", FeedController)
-
 import FlashController from "./flash_controller"
 application.register("flash", FlashController)
 
--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -2,5 +2,4 @@ class Buddy < ApplicationRecord
   has_one :user, dependent: :destroy
   # belongs_to :user
   has_many_attached :photos
-  CATEGORIES = ["Ruby", "JavaScript", "CSS"]
 end
